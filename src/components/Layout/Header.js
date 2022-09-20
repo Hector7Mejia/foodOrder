25943fe588306604fac53541166c452255a0c3e0
Hector Mejia <mejia7hector@gmail.com>
2022-09-20 11:05:57 -0500

    header.js

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import Header from 'Header.module.css';
+import {Fragment} from "react";
+
+const Header = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+};
+
+export default Header;
